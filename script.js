2ff932041b5a76d8a65cddfa83c1a5084eab4c15
Andre Lumagbas <andre.lumagbas11@gmail.com>
2026-05-03 01:16:50 -0400

    Test6.8

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function getMovie() {
+    const movieTitle = movieInput.value.trim();
 
+if (movieName === '') {
+    movieResults.innerHTML = '<p>Please enter a movie title.</p>';
+    return;
+}
+    
